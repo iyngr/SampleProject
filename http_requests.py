bd10d6727125c55c947c5cd5bfe1e17c9eea8843
Keshava Alur <iyngr@live.com>
2021-05-08 17:32:05 +0530

    Modified requests.py

--- http_requests.py
+++ http_requests.py
@@ -1,5 +1,6 @@
 # Requests Intro
 import requests
+import simplejson as json
 from io import BytesIO
 from PIL import Image
 
@@ -15,7 +16,8 @@ r = requests.get("https://bing.com/search", params=params)
 print("Status", r.status_code, r.url)
 
 # Pillow Image processing Library (PIL)
-r = requests.get("http://3.bp.blogspot.com/-MNJU7nTcF7w/UVKfKSrqHnI/AAAAAAAAAA4/DtCEkOlQcH8/s1600/Emperor+Penguin+Looking+Awesome.jpg")
+r = requests.get(
+    "http://3.bp.blogspot.com/-MNJU7nTcF7w/UVKfKSrqHnI/AAAAAAAAAA4/DtCEkOlQcH8/s1600/Emperor+Penguin+Looking+Awesome.jpg")
 print(r.status_code)
 image = Image.open(BytesIO(r.content))
 path = "./image.jpg" + image.format
@@ -27,8 +29,24 @@ except IOError:
 
 # POST Data
 
-my_data = {"name1":"Keshava","email":"info@keshava.com"}
-r = requests.post("https://w3schools.com/php/welome.php", data=my_data)
+my_data = {"name": "Keshava", "email": "info@keshava.com"}
+r = requests.post("https://tryphp.w3schools.com/demo/welcome.php", data=my_data)
 
-f = open("myfile.html","w+")
+# Headers in requests
+headers = {"Content-Type":"application/text"}
+print(r.headers)
+
+f = open("myfile.html", "w+")
 f.write(r.text)
+
+# Post JSON Data
+
+url = "http://www.googleapis.com/urlshortener/v1/url"
+payload = {"longUrl": "http://www.example.com"}
+headers = {"Content-Type": "application/json"}
+r = requests.post(url, json=payload, headers=headers)
+
+print(json.loads(r.text))
+
+
+
